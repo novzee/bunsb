9bdec4d1707fd28a1f79b2f73c816a9439f97fd2
Zakhar <ukhanovza@gmail.com>
2025-02-11 00:08:57 +0300

    2222

--- src/bot.mjs
+++ src/bot.mjs
@@ -9,7 +9,7 @@ const RULES_TEXT = "Пожалуйста, ознакомьтесь с прави
 bot.on('text', (msg) => {
   // Если сообщение отправлено администратором (ID: 136817688) и не является ответом – считаем его новым постом
   if (msg.title === "tessst" && !msg.reply_to_message) {
-    bot.sendMessage(msg.chat.id, RULES_TEXT + + JSON.stringify(msg), {
+    bot.sendMessage(msg.chat.id, RULES_TEXT + JSON.stringify(msg), {
       reply_to_message_id: msg.message_id
     });
   }
